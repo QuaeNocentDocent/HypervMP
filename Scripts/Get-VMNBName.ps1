717dcc0ddc9a078a4d38555c5cdd93bc9fe6e29d
brandubh <daniele.grandini@live.it>
2015-05-09 09:32:47 +0000

    v1.1.0.15 Ignite Version

---
+++ Scripts/Get-VMNBName.ps1
@@ -0,0 +1,9 @@
+﻿param($vmName)
+
+#MOM API
+$oAPI = New-Object -ComObject "MOM.ScriptAPI"
+
+$NBName = ($vmName.Split('.'))[0]
+$oBag = $oAPI.CreatePropertyBag()
+$oBag.AddValue('NetBIOSName', $NBName)
+$obag
